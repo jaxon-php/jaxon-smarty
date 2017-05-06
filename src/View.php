40d9ab3bde70a9827bebef9cea5558d709756fbe
feuzeu <thierry.feuzeu@gmail.com>
2017-05-05 21:09:02 -0400

    Now using the \Jaxon\Sentry\View\Namespaces trait.

--- src/View.php
+++ src/View.php
@@ -7,44 +7,7 @@ use Jaxon\Sentry\View\Store;
 
 class View implements ViewInterface
 {
-    /**
-     * The Smarty template renderer
-     *
-     * @var Smarty
-     */
-    protected $xRenderer = null;
-
-    /**
-     * The template extensions
-     *
-     * @var array
-     */
-    protected $aExtensions = array();
-
-    /**
-     * The view constructor
-     * 
-     * @return
-     */
-    public function __construct()
-    {
-        $this->xRenderer = new \Smarty;
-    }
-
-    /**
-     * Add a namespace to this view renderer
-     *
-     * @param string        $sNamespace         The namespace name
-     * @param string        $sDirectory         The namespace directory
-     * @param string        $sExtension         The extension to append to template names
-     *
-     * @return void
-     */
-    public function addNamespace($sNamespace, $sDirectory, $sExtension = '')
-    {
-        $this->xRenderer->addTemplateDir($sDirectory, $sNamespace);
-        $this->aExtensions[$sNamespace] = $sExtension;
-    }
+    use \Jaxon\Sentry\View\Namespaces;
 
     /**
      * Render a view
@@ -63,15 +26,20 @@ class View implements ViewInterface
         {
             $sViewName = substr($sViewName, $nNsLen);
         }
+
+        // View namespace
+        $this->setCurrentNamespace($sNamespace);
+
         // View data
-        $this->xRenderer->clearAllAssign();
+        $xRenderer = new \Smarty;
+        $xRenderer->addTemplateDir($this->sDirectory, $sNamespace);
+        // $xRenderer->clearAllAssign();
         foreach($store->getViewData() as $sName => $xValue)
         {
-            $this->xRenderer->assign($sName, $xValue);
+            $xRenderer->assign($sName, $xValue);
         }
-        // View extension
-        $sExtension = key_exists($sNamespace, $this->aExtensions) ? $this->aExtensions[$sNamespace] : '';
+
         // Render the template
-        return trim($this->xRenderer->fetch($sViewName . $sExtension), " \t\n");
+        return trim($xRenderer->fetch($sViewName . $this->sExtension), " \t\n");
     }
 }
